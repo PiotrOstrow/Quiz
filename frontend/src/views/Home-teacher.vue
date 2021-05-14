b0c56df88c37c96843638f97fb028a688fc12b30
Piotr Ostrowski <piotr.ostrowski.se@gmail.com>
2021-05-14 11:12:06 +0200

    added some teacher page content

--- frontend/src/views/Home-teacher.vue
+++ frontend/src/views/Home-teacher.vue
@@ -1,16 +1,29 @@
 <template>
 <div>
-  <p> WELCOME TEACHERS </p>
+  <h1> WELCOME TEACHERS :) </h1>
+
+  <h2> Your students: </h2>
+
+    <p v-for="(student, index) in students" v-bind:key="index">
+      {{ student.name }}
+    </p>
 
 </div>
 </template>
 
 <script>
 export default {
-name: "Home-teacher.vue"
+  name: "Home-teacher.vue",
+  data: function() {
+    return {
+      students: [{name: 'Bob'}, {name: 'Bob2'}, {name: 'Bob3'}, {name: 'Bob4'}, {name: 'Bob5'}]
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+h1, h2, h3, p{
+  text-align: center;
+}
 </style>
